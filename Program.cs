bbf362cdfc638bbb60714ff001490f3049225ecc
unknown <vasanthgamer2000@gmail.com>
2023-07-11 11:55:07 +0530

    Example of data type

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppDataType
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Value Type: int, float, double, 
+            //Reference Type: string, object 
+            //char myChar;
+            //int num;
+            //double num2;
+            //char myChar;
+
+            //object obj;
+            //string str;
+
+            //int num = 5;//Value Type
+            //Console.WriteLine(num); 
+            //object obj; //Reference Type
+            //obj = num;//Boxing
+            //Console.WriteLine(obj);
+
+            //int num2;
+            //num2 = (int)obj; //UnBoxing
+            //Console.WriteLine(num2);
+
+            //Console.ReadKey();
+
+            
+        }
+    }
+}
